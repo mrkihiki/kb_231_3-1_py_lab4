22a13beca568fe7642e147dc1c17732093528180
mrkihiki <mrkihiki@yandex.ru>
2025-10-19 15:20:19 +0700

    Form2

--- Form2.py
+++ Form2.py
@@ -3,8 +3,8 @@ import sqlite3
 from PyQt6.QtWidgets import QApplication, QMainWindow, QFileDialog, QTableWidgetItem, QMessageBox
 
 from Form2_ui import Ui_MainWindow as Form2_ui
-from Form2_1_ui import Ui_MainWindow as Form2_1_ui
 from Form2_1 import Form2_1
+
 
 class Form2(QMainWindow):
     def __init__(self, *args):
@@ -25,7 +25,10 @@ class Form2(QMainWindow):
             lambda: (
                 self.form.close(),
                 setattr(self, 'bool_new', False), setattr(self, 'form', Form2_1(self)), self.form.show(),
-                setattr(self, 'bool', True)) if int(self.ui.spinBox.text()) in [item[0] for item in self.res] else None)
+                setattr(self, 'bool', True)) if int(self.ui.spinBox.text()) in [item[0] for item in
+                                                                                self.res] else QMessageBox.warning(
+                self, '', f"Нету записи с id = {self.ui.spinBox.text()}",
+                QMessageBox.StandardButton.Ok))
         self.ui.pushButton_3.clicked.connect(
             lambda: (
                 self.form.close(),
@@ -37,7 +40,8 @@ class Form2(QMainWindow):
         # который ввели в текстовое поле
         query = "SELECT * FROM films"
         self.res = self.connection.cursor().execute(query).fetchall()
-        self.names = [item[0] for item in self.connection.cursor().execute("SELECT name FROM pragma_table_info('films')").fetchall()]
+        self.names = [item[0] for item in
+                      self.connection.cursor().execute("SELECT name FROM pragma_table_info('films')").fetchall()]
         # Заполним размеры таблицы
         self.ui.tableWidget.setColumnCount(5)
         self.ui.tableWidget.setRowCount(0)
@@ -47,7 +51,7 @@ class Form2(QMainWindow):
             self.ui.tableWidget.setRowCount(
                 self.ui.tableWidget.rowCount() + 1)
             for j, elem in enumerate(row):
-                if j!=3:
+                if j != 3:
                     self.ui.tableWidget.setItem(
                         i, j, QTableWidgetItem(str(elem)))
                 else:
@@ -56,7 +60,7 @@ class Form2(QMainWindow):
 
     def delete_elem(self):
         id = self.ui.spinBox.text()
-        if id not in [item[0] for item in self.res]:
+        if id not in [str(item[0]) for item in self.res]:
             return
         # Спрашиваем у пользователя подтверждение на удаление элемента
         valid = QMessageBox.question(
@@ -66,8 +70,9 @@ class Form2(QMainWindow):
         # Не забываем зафиксировать изменения
         if valid == QMessageBox.StandardButton.Yes:
             cur = self.connection.cursor()
-            cur.execute("DELETE FROM films WHERE id = ?", (id, ))
+            cur.execute("DELETE FROM films WHERE id = ?", (id,))
             self.connection.commit()
+            self.select_data()
 
     def closeEvent(self, event):
         if self.bool:
--- Form2_1.py
+++ Form2_1.py
@@ -1,8 +1,10 @@
 import sqlite3
+from datetime import date
 
 from PyQt6.QtWidgets import QApplication, QMainWindow, QFileDialog, QTableWidgetItem, QMessageBox
 
 from Form2_1_ui import Ui_MainWindow as Form2_1_ui
+
 
 class Form2_1(QMainWindow):
     def __init__(self, *args):
@@ -11,6 +13,7 @@ class Form2_1(QMainWindow):
         self.ui.setupUi(self)
         self.parent = args[-1]
         self.id = int(self.parent.ui.spinBox.text())
+        self.ui.spinBox.setMaximum(date.today().year)
         first_elements = [item[0] for item in self.parent.res]
         self.ui.comboBox.addItems(list(self.parent.genre.values()))
         if not self.parent.bool_new:
@@ -19,32 +22,46 @@ class Form2_1(QMainWindow):
             self.ui.spinBox_2.setValue(self.parent.res[first_elements.index(int(self.parent.ui.spinBox.text()))][4])
             self.ui.comboBox.setCurrentText(
                 self.parent.genre.get(self.parent.res[first_elements.index(int(self.parent.ui.spinBox.text()))][3]))
-        self.ui.pushButton.clicked.connect(lambda: self.run(first_elements))
+        self.ui.pushButton.clicked.connect(self.run)
         self.ui.pushButton_2.clicked.connect(self.close)
 
-    def run(self, first_elements):
+    def run(self):
+        params = [
+            self.ui.lineEdit.text(),
+            int(self.ui.spinBox.text()),
+            int(next((k for k, v in self.parent.genre.items() if v == self.ui.comboBox.currentText()), None)),
+            int(self.ui.spinBox_2.text())
+        ]
+        params[0] = " ".join(params[0].split())
+        print(params)
+        if "" in params:
+            QMessageBox.warning(
+                self, '', "Заполните все столбцы",
+                QMessageBox.StandardButton.Ok)
+            return
         cur = self.parent.connection.cursor()
         if self.parent.bool_new:
-            print(max(first_elements))
+            que = "INSERT INTO films ("
+            que += ", ".join([f"{col}" for col in self.parent.names[1:]])
+            que += ") VALUES ("
+            que += ", ".join(["? " for col in self.parent.names[1:]])
+            que += ")"
+            cur.execute(que, params)
+            self.parent.connection.commit()
+            self.parent.select_data()
+            self.close()
         else:
             que = "UPDATE films SET\n"
             que += ", ".join([f"{col} = ?" for col in self.parent.names[1:]])
             que += " WHERE id = ?"
             print(que)
-            params = [
-                self.ui.lineEdit.text(),
-                int(self.ui.spinBox.text()),
-                int(next((k for k, v in self.parent.genre.items() if v == self.ui.comboBox.currentText()), None)),
-                int(self.ui.spinBox_2.text()),
-                int(self.id)  # id для WHERE должен быть последним
-            ]
-            cur.execute(que, params)
+            cur.execute(que, (params + [int(self.id)]))
             self.parent.connection.commit()
-        self.parent.select_data()
-        self.close()
+            self.parent.select_data()
+            self.close()
 
     def closeEvent(self, event):
-        #self.connection.close()
+        # self.connection.close()
         self.parent.bool = False
         event.accept()  # Разрешаем закрытие
         self.parent.show()
